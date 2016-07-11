51779e1b0c87a9283313f2b3251f7f66a6550552
Randy Davis <randy_davis@outlook.com>
2016-07-11 00:44:17 -0500

    update controller to properly use $http injection module; get hard-coded data from server

--- public/features/services/controller.js
+++ public/features/services/controller.js
@@ -1,8 +1,16 @@
 /** Created by randy on 7/10/16. */
 angular.module('app', [])
-    .controller('ServicesCtrl', function ($scope) {
+    .controller('ServicesCtrl', ['$scope', '$http', function ($scope, $http) {
         console.log('Hello from ServicesCtrl');
         $scope.message = "Hello from controller!";
 
-        
-    })
+
+
+        $scope.renderServiceClients = function (response) {
+            $scope.serviceClients = response;
+        }
+
+        // get all
+        $http.get("/serviceClients")
+            .success($scope.renderServiceClients);
+    }]);
--- server.js
+++ server.js
@@ -6,6 +6,19 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-//app.get();
+app.get("/serviceClients", function (req, res) {
+    var svc1 = {
+        name: "LinkedIn"
+    };
+    var svc2 = {
+        name: "Rotten Tomatoes"
+    };
+    var svc3 = {
+        name: "IMDB"
+    };
+
+    var serviceClients = [svc1, svc2, svc3];
+    res.json(serviceClients);
+});
 
 app.listen(3000);
